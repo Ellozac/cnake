743d0e1cf9db80147a2922fd8c7c6c7436325c6a
Ellozac <zlsy108@gmail.com>
2023-12-02 22:13:46 +1300

    prints with snake head in center

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
 int main() { 
     // Check OS running the program
     // os 1 is linux, 2 is windows
@@ -15,7 +16,7 @@ int main() {
     #endif
 
 
-    // characters used to display the snake and apple and border
+    // characters/assets used to display the snake and apple and border
     char snakeHead = '0';
     char snakeBody = 'o';
     char apple = 'a';
@@ -24,16 +25,18 @@ int main() {
     int arena[2];
     arena[0] = 56;
     arena[1] = 16;
-
+    // initial length of snake
     int length = 1;
+    // dynamic memory allocation for snake position x and y in Heap
     int* snakePosX = (int *)malloc(sizeof(int) * length);
     int* snakePosY = (int *)malloc(sizeof(int) * length);
     
+
+    // set snake position to center of screen for start of game
     snakePosX[0] = arena[0] / 2;
     snakePosY[0] = arena[1] / 2;
 
-
-
+    // starting the game, Whilst gameState is 1 the game is running if 0 game stops 
     int gameState = 1;
     while (gameState) {
         // Clear Terminal based on OS 
@@ -59,15 +62,19 @@ int main() {
         }
         for (int i = 0; i < arena[1]; i++) {
             printf("\n");
+            
             for (int k = 0; k < arena[0]; k++) {
+                // logic for what character to output to console
+                if (snakePosX[0] == k && snakePosY[0] == i) {
+                    printf("%c", snakeHead);
+                    continue;
+                };
+                for (int j = 1; j <= length; j++) if (snakePosX[j] == k && snakePosY[j] == k) {
+                    printf("%c", snakeBody);
 
-                if (k == 0 || k == arena[0] - 1) 
-                {
-                    printf("%c", arenaBorder);
                 }
-                else {
-                    printf(" ");
-                }
+                else if (k == 0 || k == arena[0] - 1) printf("%c", arenaBorder);
+                else printf(" ");
             }
             
         }
@@ -79,5 +86,7 @@ int main() {
         gameState = 0;
     }
 
+    free(snakePosX);
+    free(snakePosY);
     return 0;
 }
